eae064b6721bd94b8d6db8aba48eed423d64a9fb
Zidane <ouldessayda@gmail.com>
2020-12-28 22:29:54 +0100

    Revert "Revert "creation de table/controller/seeder/modele pour la banniere""
    This reverts commit 2426d1d953ce832dd9f05c03c02f90c43e09a043.

---
+++ app/Http/Controllers/BanniereController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Banniere;
+use Illuminate\Http\Request;
+
+class BanniereController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Banniere  $banniere
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Banniere $banniere)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Banniere  $banniere
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Banniere $banniere)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Banniere  $banniere
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Banniere $banniere)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Banniere  $banniere
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Banniere $banniere)
+    {
+        //
+    }
+}
